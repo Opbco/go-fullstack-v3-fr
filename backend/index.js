6aaddc2282d0dc60876d556bc197db1573578bbb
Opbco <brice.mich@gmail.com>
2022-10-10 09:08:10 +0100

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
 const server = http.createServer((req, res)=>{
-    res.end('Mon premier javascript server');
+    res.end('Mon premier javascript server by OPBco');
 });
 
 server.listen(process.env.PORT || 3000);
